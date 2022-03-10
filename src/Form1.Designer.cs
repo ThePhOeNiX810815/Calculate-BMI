31acb0522d479d8649bca7c22713115abde2c84e
Peter Machaj <phoenixvm@gmail.com>
2022-03-10 12:51:01 +0100

    Solution Commit

---
+++ src/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace CalculateBMI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown_Height = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown_Weight = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button_Calculate = new System.Windows.Forms.Button();
+            this.label_BMI_Result = new System.Windows.Forms.Label();
+            this.label_Overall_Result = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Height)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Weight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.DarkGray;
+            this.label1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(469, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculate Body Mass Index";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(6, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(457, 187);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 211);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(469, 212);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "BMI Legend";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label_Overall_Result);
+            this.groupBox2.Controls.Add(this.label_BMI_Result);
+            this.groupBox2.Controls.Add(this.button_Calculate);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.numericUpDown_Weight);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.numericUpDown_Height);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 51);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(469, 134);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Calculate BMI";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Gainsboro;
+            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(17, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Select Your Height";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numericUpDown_Height
+            // 
+            this.numericUpDown_Height.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.numericUpDown_Height.Location = new System.Drawing.Point(152, 25);
+            this.numericUpDown_Height.Maximum = new decimal(new int[] {
+            250,
+            0,
+            0,
+            0});
+            this.numericUpDown_Height.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown_Height.Name = "numericUpDown_Height";
+            this.numericUpDown_Height.Size = new System.Drawing.Size(44, 20);
+            this.numericUpDown_Height.TabIndex = 2;
+            this.numericUpDown_Height.Value = new decimal(new int[] {
+            175,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(196, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "cm";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(196, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "kg";
+            // 
+            // numericUpDown_Weight
+            // 
+            this.numericUpDown_Weight.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.numericUpDown_Weight.Location = new System.Drawing.Point(152, 48);
+            this.numericUpDown_Weight.Maximum = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.numericUpDown_Weight.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown_Weight.Name = "numericUpDown_Weight";
+            this.numericUpDown_Weight.Size = new System.Drawing.Size(44, 20);
+            this.numericUpDown_Weight.TabIndex = 5;
+            this.numericUpDown_Weight.Value = new decimal(new int[] {
+            80,
+            0,
+            0,
+            0});
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Gainsboro;
+            this.label5.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(17, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(132, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Select Your Weight";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button_Calculate
+            // 
+            this.button_Calculate.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_Calculate.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Calculate.Location = new System.Drawing.Point(224, 25);
+            this.button_Calculate.Name = "button_Calculate";
+            this.button_Calculate.Size = new System.Drawing.Size(85, 43);
+            this.button_Calculate.TabIndex = 7;
+            this.button_Calculate.Text = "CALCULATE";
+            this.button_Calculate.UseVisualStyleBackColor = true;
+            this.button_Calculate.Click += new System.EventHandler(this.button_Calculate_Click);
+            // 
+            // label_BMI_Result
+            // 
+            this.label_BMI_Result.BackColor = System.Drawing.Color.Gainsboro;
+            this.label_BMI_Result.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_BMI_Result.ForeColor = System.Drawing.Color.Black;
+            this.label_BMI_Result.Location = new System.Drawing.Point(323, 25);
+            this.label_BMI_Result.Name = "label_BMI_Result";
+            this.label_BMI_Result.Size = new System.Drawing.Size(132, 43);
+            this.label_BMI_Result.TabIndex = 8;
+            this.label_BMI_Result.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label_Overall_Result
+            // 
+            this.label_Overall_Result.BackColor = System.Drawing.Color.Silver;
+            this.label_Overall_Result.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Overall_Result.ForeColor = System.Drawing.Color.White;
+            this.label_Overall_Result.Location = new System.Drawing.Point(16, 86);
+            this.label_Overall_Result.Name = "label_Overall_Result";
+            this.label_Overall_Result.Size = new System.Drawing.Size(439, 28);
+            this.label_Overall_Result.TabIndex = 9;
+            this.label_Overall_Result.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 435);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(513, 478);
+            this.MinimumSize = new System.Drawing.Size(513, 478);
+            this.Name = "Form1";
+            this.Text = "Calculate BMI";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Height)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Weight)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label_Overall_Result;
+        private System.Windows.Forms.Label label_BMI_Result;
+        private System.Windows.Forms.Button button_Calculate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown_Weight;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown_Height;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ src/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CalculateBMI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Calculate_Click(object sender, EventArgs e)
+        {
+            double selectedWeight_In_KG = (double)numericUpDown_Weight.Value;
+            double selectedHeight_In_CM = (double)numericUpDown_Height.Value;
+            double selectedHeight_In_M = selectedHeight_In_CM / 100;
+
+            double calculatedBMI = selectedWeight_In_KG / (selectedHeight_In_M * selectedHeight_In_M);
+
+            label_BMI_Result.Text = "Your BMI is " + Math.Round(calculatedBMI, 2);
+
+            if (calculatedBMI <= 18.5)
+            {
+                label_Overall_Result.BackColor = Color.DeepSkyBlue;
+                label_Overall_Result.Text = "You are UNDERWEIGHT";
+            }
+            else if (calculatedBMI <= 25)
+            {
+                label_Overall_Result.BackColor = Color.LimeGreen;
+                label_Overall_Result.Text = "Your weight is NORMAL";
+            }
+            else if (calculatedBMI <= 30)
+            {
+                label_Overall_Result.BackColor = Color.Gold;
+                label_Overall_Result.Text = "You are OVERWEIGHT";
+            }
+            else if (calculatedBMI > 30)
+            {
+                label_Overall_Result.BackColor = Color.Red;
+                label_Overall_Result.Text = "You are OBESE";
+            }
+        }
+    }
+}
